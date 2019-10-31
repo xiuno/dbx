90ee7599864f787b7c8a75c86f63ba75518aaa85
hm <hm@johnwick.io>
2019-10-31 17:14:38 +0800

    CQL Consistency default

--- dbx.go
+++ dbx.go
@@ -272,8 +272,9 @@ func OpenFile(filePath string) *os.File {
 func NewCQLSession(hosts []string, keySpace string) (*gocql.Session, error) {
 	cluster := gocql.NewCluster(hosts...) //  "192.168.0.129:9042"
 	cluster.Keyspace = keySpace           // dbname "mycas"
-	cluster.Consistency = gocql.Consistency(1)
-	cluster.NumConns = 3	// 并发连接数，不要开太多，否则连接时间很长！
+	//cluster.Consistency = gocql.Consistency(0)	// https://teddymaef.github.io/learncassandra/cn/replication/turnable_consistency.html
+	//cluster.Consistency = gocql.Quorum	// https://teddymaef.github.io/learncassandra/cn/replication/turnable_consistency.html
+	cluster.NumConns = 4	// 每个主机的并发连接数，不要开太多，否则连接时间很长！
 	cluster.ConnectTimeout = 600 * time.Second
 	cluster.Timeout = 600 * time.Second
 	return cluster.CreateSession()
